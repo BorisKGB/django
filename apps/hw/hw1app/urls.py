664ee2d155c710eb84b085eb0a171756df7e3aaf
BorisKGB <flot3.sky@yandex.ru>
2024-01-30 21:44:04 +0300

    add task solution

--- apps/hw/hw1app/urls.py
+++ apps/hw/hw1app/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main_page, name='main_page'),
+    path('about', views.about_page, name='about_page'),
 ]
--- apps/hw/hw1app/views.py
+++ apps/hw/hw1app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+LINKS = [
+    {
+        'name': 'Main page',
+        'url_for': 'main_page'
+    },
+    {
+        'name': 'About me',
+        'url_for': 'about_page'
+    }
+]
+
+
+def main_page(request):
+    data = {
+        'title': 'Main page',
+    }
+    return render(request, 'base.html', {**data, 'links': LINKS})
+
+
+def about_page(request):
+    data = {
+        'title': 'About me',
+    }
+    return render(request, 'base.html', {**data, 'links': LINKS})
