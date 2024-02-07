522b09e386e077ca14f1f035b6287c98413c28ec
BorisKGB <flot3.sky@yandex.ru>
2024-02-07 21:07:51 +0300

    add client orders view, url and template

--- apps/hw/shopapp/views.py
+++ apps/hw/shopapp/views.py
@@ -7,4 +7,5 @@ from django.shortcuts import render
 def client_orders(request: HttpResponse, client_id: int):
     # client = get_object_or_404(ClientModel, pk=client_id)
     orders = OrderModel.objects.filter(client__id=client_id)
-    return render(request, 'shopapp/client_orders.html', context={'orders': orders})
+    return render(request, 'shopapp/client_orders.html',
+                  context={'orders': orders, 'client_id': client_id})
